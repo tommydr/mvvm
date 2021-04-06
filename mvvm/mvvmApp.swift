65a98b3590274cdc631b7c82cd592885610e71ad
Tommy den Reijer <t.ommy@hotmail.nl>
2021-04-06 17:59:42 +0200

    Initial Commit

---
+++ mvvm/mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmApp.swift
+//  mvvm
+//
+//  Created by Tommy den Reijer on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
